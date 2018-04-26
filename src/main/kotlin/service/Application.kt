4a9c47247c54dec06e742a01bc0f26df3be39ee4
bkoruznjak <borna.koruznjak@gmail.com>
2018-04-26 21:08:14 +0200

    - updated main so spring boot doesn't complain

--- src/main/kotlin/service/Application.kt
+++ src/main/kotlin/service/Application.kt
@@ -4,8 +4,13 @@ import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 
 @SpringBootApplication
-class Application
+class Application {
 
-fun main(args: Array<String>) {
-    SpringApplication.run(Application::class.java, *args)
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            SpringApplication.run(Application::class.java, *args)
+        }
+    }
 }
+
--- src/main/kotlin/service/controller/CurrentSongController.kt
+++ src/main/kotlin/service/controller/CurrentSongController.kt
@@ -8,8 +8,8 @@ import service.data.CurrentSong
 @RestController
 class CurrentSongController {
 
-    @GetMapping("/current")
-    fun getCurrentSong(@RequestParam(value = "client", defaultValue = "0") clientId: String) =
+    @GetMapping("/playing")
+    fun getCurrentSong(@RequestParam(value = "client", defaultValue = "0") clientId: String,
+                       @RequestParam(value = "entries", defaultValue = "10") amountOfEntries: String) =
             CurrentSong(1, "Nedeljko Bajić Baja", "Oficir s ružom", "http://tekstomanija.com/wp-content/uploads/2016/08/Tekstomanija-Nedeljko-Baji%C4%87-Baja-Ljubavnik.jpg?x38675")
-
 }
